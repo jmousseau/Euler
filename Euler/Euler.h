4fcae6ae76af7055d0c03ca973b75b16291d35ae
Jack Mousseau <jackmousseau@gmail.com>
2020-05-28 20:05:48 -0700

    Initial commit

---
+++ Euler/Euler.h
@@ -0,0 +1,15 @@
+//
+//  Euler.h
+//  Euler
+//
+//  Created by Jack Mousseau on 5/28/20.
+//  Copyright © 2020 Jack Mousseau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Euler Project version number.
+FOUNDATION_EXPORT double EulerVersionNumber;
+
+//! Euler project version string.
+FOUNDATION_EXPORT const unsigned char EulerVersionString[];
